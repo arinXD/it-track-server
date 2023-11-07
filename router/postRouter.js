902ba7b3acab12dbda33e84ed3af6b69adb1c81d
ArinSwagg <rakuzanoat@gmail.com>
2023-11-07 08:37:27 +0700

    add server error handle

--- router/postRouter.js
+++ router/postRouter.js
@@ -4,9 +4,13 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const Post = prisma.post
 
-router.get('/', async (rqe, res)=>{
-    const posts = await Post.findMany()
-    return res.status(200).json({posts})
+router.get('/', async (rqe, res) => {
+    try {
+        const posts = await Post.findMany()
+        return res.status(200).json({ posts })
+    } catch (err) {
+        return res.status(500).json({ message: "server error" })
+    }
 })
 
 module.exports = router
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -5,47 +5,63 @@ const prisma = new PrismaClient()
 const User = prisma.user
 
 router.get('/', async (req, res, next) => {
-    const users = await User.findMany()
-    return res.status(200).json({ users })
+    try {
+        const users = await User.findMany()
+        return res.status(200).json({ users })
+    } catch (err) {
+        return res.status(500).json({ message: "server error" })
+    }
 })
 
 router.get('/:id', async (req, res, next) => {
-    const id = parseInt(req.params.id)
-    if(!id){
-        return res.status(400).json({ message: `provide user id '/users/:id'` })
-    }
-    const users = await User.findUnique({
-        where: {
-            id: id
+    try {
+        const id = parseInt(req.params.id)
+        if (!id) {
+            return res.status(400).json({ message: `provide user id '/users/:id'` })
         }
-    })
-    return res.status(200).json({ users })
+        const users = await User.findUnique({
+            where: {
+                id: id
+            }
+        })
+        return res.status(200).json({ users })
+    } catch (err) {
+        return res.status(500).json({ message: "server error" })
+    }
 })
 
 router.get('/all/posts', async (req, res, next) => {
-    const users = await User.findMany({
-        include: {
-            posts: true,
-            profile: true,
-        },
-    })
-    return res.json({ users })
+    try {
+        const users = await User.findMany({
+            include: {
+                posts: true,
+                profile: true,
+            },
+        })
+        return res.json({ users })
+    } catch (err) {
+        return res.status(500).json({ message: "server error" })
+    }
 })
 
 router.get('/:id/posts', async (req, res, next) => {
-    const id = parseInt(req.params.id)
-    if(!id){
-        return res.status(400).json({ message: `provide user id '/users/:id'` })
-    }
-    const users = await User.findUnique({
-        where: {
-            id: id
-        },
-        include:{
-            posts: true
+    try {
+        const id = parseInt(req.params.id)
+        if (!id) {
+            return res.status(400).json({ message: `provide user id '/users/:id'` })
         }
-    })
-    return res.status(200).json({ users })
+        const users = await User.findUnique({
+            where: {
+                id: id
+            },
+            include: {
+                posts: true
+            }
+        })
+        return res.status(200).json({ users })
+    } catch (err) {
+        return res.status(500).json({ message: "server error" })
+    }
 })
 
 module.exports = router
