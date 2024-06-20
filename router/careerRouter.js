ca1c86afeb704b6d16e2fb3f6740f1df4ee8a964
nuttttx <101942998+arinXD@users.noreply.github.com>
2024-06-20 16:42:44 +0700

    career management

--- router/careerRouter.js
+++ router/careerRouter.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const isAuth = require("../middleware/authMiddleware")
 const isAdmin = require("../middleware/adminMiddleware");
 const { careerCreateUploader, careerUpdateUploader } = require('../utils/uploader');
-const { createCareer, getAllCareers, updateCareer, deleteMultipleCareer, getCareerByTrack } = require('../controller/careerController');
+const { createCareer, getAllCareers, updateCareer, deleteMultipleCareer, getCareerByTrack,
+     getCareerByID
+} = require('../controller/careerController');
 
 // GET
 router.get("/", getAllCareers)
-router.get("/:track", getCareerByTrack)
+router.get("/:id", getCareerByID)
+router.get("/tracks/:track", getCareerByTrack)
 
 // POST
-router.post("/", careerCreateUploader.single("image"), createCareer)
+router.post("/",careerCreateUploader.single("image"), createCareer)
 
 // PUT
-router.put("/:id", careerUpdateUploader.single("image"), updateCareer)
+router.put("/:id",careerUpdateUploader.single("image"), updateCareer)
 
 // DELETE
 router.delete("/multiple", deleteMultipleCareer)
