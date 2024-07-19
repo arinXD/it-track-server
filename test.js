f8010d0cad71be353b244968e2c5389ce74f15c9
arinxd <rakuzanoat@gmail.com>
2024-07-19 19:49:39 +0700

    Install base64 module.

---
+++ test.js
@@ -0,0 +1,7 @@
+const { base64encode, base64decode } = require('nodejs-base64');
+
+const baseString = "awdlaw,fpmawd1a561wd68aw1d@aw-dp-_Ada+a-wd!awd,awd415641651asdaw0awdaOKSAXC_)OW_Q"
+
+const encoded = base64encode(baseString);
+const decoded = base64decode(encoded); 
+
