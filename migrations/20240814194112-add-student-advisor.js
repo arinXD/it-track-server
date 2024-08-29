ba33b3e3e7aa206e94c996e76a828485fc454243
kiok127523 <kiok127523@gmail.com>
2024-08-30 01:23:50 +0700

    Create StudentVerifyApprovements table.

--- migrations/20240814194112-add-student-advisor.js
+++ migrations/20240814194112-add-student-advisor.js
@@ -20,6 +20,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.removeConstraint('Students', 'advisor');
+        await queryInterface.removeColumn('Students', 'advisor');
     }
 };
